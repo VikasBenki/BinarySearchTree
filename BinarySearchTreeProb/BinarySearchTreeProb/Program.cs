2ff9abb64223c12caa9f497a5e72a09e9f14a3ff
VikasBenki <vikasmsbenki@gmail.com>
2022-02-09 13:56:21 +0530

    [VikasBenki] Adding the CreatesimpleBst

--- BinarySearchTreeProb/BinarySearchTreeProb/Program.cs
+++ BinarySearchTreeProb/BinarySearchTreeProb/Program.cs
@@ -11,7 +11,21 @@ namespace BinarySearchTreeProb
         static void Main(string[] args)
         {
             Console.WriteLine("Hello welcome to Binary search tree problem ");
+            ViewBST();
             Console.ReadLine();
         }
+        /// <summary>
+        /// Method to Initialize Nodes.
+        /// </summary>
+        public static void ViewBST()
+        {
+            MyBinaryTree<int> tree = new MyBinaryTree<int>();
+            tree.CreateNode(56);
+            tree.CreateNode(30);
+            tree.CreateNode(70);
+            Console.WriteLine("Display Nodes present in Binary Search Tree");
+            tree.Display();
+        }
     }
+                
 }
