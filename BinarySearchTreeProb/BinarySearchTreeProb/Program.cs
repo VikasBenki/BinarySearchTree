a65657a25645a5f931b58c1b15e693e6b6829af6
VikasBenki <vikasmsbenki@gmail.com>
2022-02-09 13:32:01 +0530

    [vikasBenki] Adding the welcome message

---
+++ BinarySearchTreeProb/BinarySearchTreeProb/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTreeProb
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello welcome to Binary search tree problem ");
+            Console.ReadLine();
+        }
+    }
+}
